e1aa3091f52ad1146a6808336cc179621d02d546
yuan29r <sheng29r@gmail.com>
2019-12-24 17:52:59 +0800

    編輯 與 烈表 實作

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'angular-tour-og-heroes';
+  title = 'Angular Tour of Heroes';
 }
---
+++ src/app/heroes/heroes.component.ts
@@ -0,0 +1,23 @@
+import { Component, OnInit } from '@angular/core';
+import { Hero } from '../Hero';
+import { HEROES } from '../mock-heroes';
+
+@Component({
+  selector: 'app-heroes',
+  templateUrl: './heroes.component.html',
+  styleUrls: ['./heroes.component.css']
+})
+
+export class HeroesComponent implements OnInit {
+  constructor() { }
+  // hero: Hero = { id: 1, name: 'Windstorm' };
+  heroes = HEROES;
+  selectedHero: Hero;
+
+  ngOnInit() {
+  }
+
+  onSelect(hero: Hero): void {
+    this.selectedHero = hero;
+  }
+}
