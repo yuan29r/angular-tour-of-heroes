6ee403b580ab0de4c9978f652720423b62b7cfb6
yuan29r <sheng29r@gmail.com>
2019-12-26 13:34:17 +0800

    路由01

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,7 +3,10 @@ import { RouterModule, Routes } from '@angular/router';
 
 import { HeroesComponent } from './heroes/heroes.component';
 
-const routes: Routes = [{ path: 'heroes', component: HeroesComponent }];
+// route清單
+const routes: Routes = [
+  { path: 'heroes', component: HeroesComponent }
+];
 
 @NgModule({
   imports: [
